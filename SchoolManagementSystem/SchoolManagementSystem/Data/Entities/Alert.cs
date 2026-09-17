6f259aa4079030344d53c1b6b01fb01a6958252a
ALCSG <andrelobograca@gmail.com>
2026-09-17 20:36:53 +0100

    Add Entity Alert for admin alerts

---
+++ SchoolManagementSystem/SchoolManagementSystem/Data/Entities/Alert.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SchoolManagementSystem.Data.Entities
+{
+    public class Alert
+    {
+        public int AlertId { get; set; }
+
+        [Required, MaxLength(500)]
+        public string Message { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+
+        public bool IsResolved { get; set; } = false;
+
+        [ForeignKey(nameof(ReportedBy))]
+        public string ReportedByUserId { get; set; }
+
+        public AppUser ReportedBy { get; set; }
+    }
+}
