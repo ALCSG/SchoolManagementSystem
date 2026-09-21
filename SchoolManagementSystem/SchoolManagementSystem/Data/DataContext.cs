bcfbd6d1d63f53cc4052ac5c6231a7287114a79e
ALCSG <andrelobograca@gmail.com>
2026-09-21 11:26:04 +0100

    Add DataContext

---
+++ SchoolManagementSystem/SchoolManagementSystem/Data/DataContext.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using SchoolManagementSystem.Data.Entities;
+
+namespace SchoolManagementSystem.Data
+{
+    public class DataContext : IdentityDbContext<AppUser>
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<ClassGroup> ClassGroups { get; set; }
+        public DbSet<Subject> Subjects { get; set; }
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+        public DbSet<Alert> Alerts { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<Course>()
+                .HasIndex(c => c.Name)
+                .IsUnique();
+
+            builder.Entity<Subject>()
+                .HasIndex(s => s.Name)
+                .IsUnique();
+        }
+    }
+}
